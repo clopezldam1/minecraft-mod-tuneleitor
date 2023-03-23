3fed66f1468903f886f978b6c73500b07457058f
clopezldam1 <clopezldam1@gmail.com>
2023-03-23 21:12:33 +0100

    [Update] Commit con pruebas Sout para ir reconociendo variables (topic: jugador hace acción sobre bloque en posicion)

--- src/main/java/es/mariaanasanz/ut7/mods/impl/Tuneleitor.java
+++ src/main/java/es/mariaanasanz/ut7/mods/impl/Tuneleitor.java
@@ -4,6 +4,7 @@ import es.mariaanasanz.ut7.mods.base.*;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ShovelItem;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.event.MovementInputUpdateEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
@@ -95,11 +96,13 @@ public class Tuneleitor extends DamMod implements IBlockBreakEvent, IServerStart
     @Override
     @SubscribeEvent
     public void onPlayerTouch(PlayerInteractEvent.RightClickBlock event) {
-        System.out.println("¡Has hecho click derecho! AAAA"); // Este metodo no lo lee
+        System.out.println("¡Has hecho click derecho! {ONCE}");
+        
         BlockPos pos = event.getPos();
         BlockState state = event.getLevel().getBlockState(pos);
         Player player = event.getEntity();
         ItemStack heldItem = player.getMainHandItem();
+        
         if (ItemStack.EMPTY.equals(heldItem)) {
             System.out.println("La mano esta vacia");
             if (state.getBlock().getName().getString().trim().toLowerCase().endsWith("log")) {
@@ -107,11 +110,48 @@ public class Tuneleitor extends DamMod implements IBlockBreakEvent, IServerStart
             }
         }
     
-        System.out.println("[event.getUseItem()]¡Has usado un objeto! OBJETO:" + event.getUseItem());
+        System.out.println("[event.getUseItem()]¡Has usado un objeto! OBJETO:" + event.getUseItem()+ "\n"+
+                                "\t"+"¿Ese objeto usado es una PALA (de cualquier tipo)? == " + event.getItemStack().getItem().toString().trim().endsWith("shovel")+"\n"+
+                                "\t"+"event.getPos() == " + event.getPos() +"\n"+
+                                "\t"+"event.getLevel() == "+ event.getLevel() +"\n"+
+                                "\t"+"event.getUseBlock() == "+event.getUseBlock() +"\n"+
+                                "\t"+"[event.getItemStack()] CUAL FUE EL OBJETO USADO == "+ event.getItemStack() +"\n"+
+                                "\t"+"[returns: InteractionHand // event.getHand()] QUE MANO HA SIDO USADA PARA LA ACCION == "+ event.getHand() +"\n"
+//                                "\t"+"" +"\n"+
+                            );
+        
         System.out.println("[BlockPos] la posicion del bloque afectado es: " + pos);
-        System.out.println("[BlockState] El estado del bloque afectado ahora es: " + state);
-        System.out.println("[Player] El jugador es: " + player);
-        System.out.println("[ItemStack] El item que sostiene el jugador es:  " + heldItem);
+        
+        System.out.println("[BlockState] El estado ANTERIOR del bloque que ha sido afectado ES (aka, WHICH BLOCK WAS TARGETED?): " + state + "\n"+
+                                "\t"+"Nombre ANTERIOR del bloque afectado: "+state.getBlock() +"\n"+
+                                "\t"+"state.getMaterial() == "+ state.getMaterial() +"\n"
+//                                "\t"+"" +"\n"+
+                            );
+        
+        System.out.println("[Player] La información sobre el jugador es: (player) " + "\n"+
+                                "\t"+"Nombre jugador == " + player.getName() + "\n"+
+                                "\t"+"Objeto que jugador lleva en mano DERECHA(MAIN_HAND) "+player.getMainHandItem() +"\n"+
+                                "\t"+"ver que tiene en los handSlots: "+player.getHandSlots() +"\n"+
+                                "\t"+"player.getBlockX() == "+player.getBlockX() +"\n"+
+                                "\t"+"player.getBlockY() == "+player.getBlockY() +"\n"+
+                                "\t"+"player.getBlockZ() == "+player.getBlockZ() +"\n"+
+                                "\t"+"saber en que posicion está el bloque where im standing: "+player.getOnPos() +"\n"+
+                                "\t"+"player.position() == "+player.position() +"\n"+
+                                "\t"+"player.getUseItem() == "+player.getUseItem() +"\n"+
+                                "\t"+"player.isHolding(event.getItemStack().getItem()) == "+player.isHolding(event.getItemStack().getItem()) +"\n"+
+                                "\t"+"saber si el jugador está sobre suelo firme: "+player.isOnGround() +"\n"+
+                                "\t"+"saber si el jugador está sprinteando: "+player.isSprinting() +"\n"+
+                                "\t"+"saber si el jugador está en el agua (SOLO SI ESTA TOCANDO EL AGUA O BAJO ELLA): "+player.isInWater() +"\n"
+//                               "\t"+"" +"\n"+
+                            );
+        System.out.println("[ItemStack] El item que sostiene el jugador es: (heldItem) " + "\n"+
+                                "\t"+"Nombre del item: "+ heldItem.getItem()+"\n"+
+                                "\t"+"Cantidad de ese item: "+ heldItem.getCount() +"\n"+
+                                "\t"+"heldItem.getDescriptionId() == "+ heldItem.getDescriptionId() +"\n"+
+                                "\t"+"heldItem.isEmpty() == "+ heldItem.isEmpty() +"\n"+
+                                "\t"+"Saber si el objeto que tiene en la mano está encantado: "+ heldItem.isEnchanted()+"\n"
+//                                "\t"+"" +"\n"+
+                            );
     
         /**
          *   LO QUE SE IMPRIME TRAS EJECUTAR EL METODO:
@@ -122,13 +162,56 @@ public class Tuneleitor extends DamMod implements IBlockBreakEvent, IServerStart
          *             [Player] El jugador es: ServerPlayer['Dev'/152, l='ServerLevel[PruebasTuneleitor]', x=21.65, y=67.00, z=12.64]
          *             [ItemStack] El item que sostiene el jugador es:  1 wooden_shovel
          *
-         *   CONCLUSIONES RESULTADOS DE LA PRUEBA:
-         *       event.getUseItem().name() == DEFAULT
+         *
+         *
+         *   CONCLUSIONES RESULTADOS DE LAS PRUEBAS:
+         *       event.getUseItem().name() == DEFAULT (??)
          *       pos ==  MutableBlockPos{x=20, y=66, z=11}
          *       state == Block{minecraft:dirt_path}
          *       player == ServerPlayer['Dev'/152, l='ServerLevel[PruebasTuneleitor]', x=21.65, y=67.00, z=12.64]
          *       heldItem == 1 wooden_shovel
+         * __________________________________________________________________
+         *     ¡Has hecho click derecho! {ONCE}
+         *
+         *     [event.getUseItem()]¡Has usado un objeto! OBJETO:DEFAULT
+         *         ¿Ese objeto es una pala? == false
+         *         event.getPos() == MutableBlockPos{x=-10, y=66, z=5}
+         *         event.getLevel() == ServerLevel[PruebasTuneleitor]
+         *         event.getUseBlock() == DEFAULT
+         *         event.getItemStack() == 1 wooden_shovel
+         *         [returns: InteractionHand] event.getHand() == MAIN_HAND
+         *
+         *     [BlockPos] la posicion del bloque afectado es: MutableBlockPos{x=-10, y=66, z=5}
+         *
+         *     [BlockState] El estado ACTUAL del bloque que ha sido afectado ahora es: Block{minecraft:grass_block}[snowy=false]
+         *         Nombre ACTUAL del bloque afectado: Block{minecraft:grass_block}
+         *         state.getMaterial() == net.minecraft.world.level.material.Material@440e28fa
+         *
+         *     [Player] La información sobre el jugador es: (player)
+         *         Nombre jugador == literal{Dev}
+         *         Objeto que jugador lleva en mano principal == 1 wooden_shovel
+         *         ver que tiene en los handSlots: [1 wooden_shovel, 1 air]
+         *         player.getBlockX() == -9
+         *         player.getBlockY() == 67
+         *         player.getBlockZ() == 4
+         *         saber en que posicion está el bloque where im standing: BlockPos{x=-9, y=66, z=4}
+         *         player.position() == (-8.118774775207585, 67.0, 4.301416443852919)
+         *         player.getUseItem() == 1 air
+         *         player.isHolding(event.getItemStack().getItem()) == true
+         *         saber si el jugador está sobre suelo firme: true
+         *         saber si el jugador está sprinteando: false
+         *         saber si el jugador está en el agua (sobre ella o bajo ella): false
+         *
+         *     [ItemStack] El item que sostiene el jugador es: (heldItem)
+         *         Nombre del item: wooden_shovel
+         *         Cantidad de ese item: 1
+         *         heldItem.getDescriptionId() == item.minecraft.wooden_shovel
+         *         heldItem.isEmpty() == false
+         *         Saber si el objeto que tiene en la mano está encantado: false
+         * ________________________________________________________________________________
+         *
          */
+   
     }
 
     @Override
